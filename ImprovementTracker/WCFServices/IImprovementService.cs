df337fbf649d547f9b38ab6fcfc385b925f221e9
cliffhyde <cliff.hyde@computacenter.com>
2014-11-05 14:01:47 +0000

    Introducing WCF

---
+++ ImprovementTracker/WCFServices/IImprovementService.cs
@@ -0,0 +1,26 @@
+﻿using ImprovementTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ImprovementTracker.WCFServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IImprovementService" in both code and config file together.
+    [ServiceContract(Namespace="Computacenter",ProtectionLevel=ProtectionLevel.None)]
+    public interface IImprovementService
+    {
+        [OperationContract]
+        IImprovement GetImprovement(int Id);
+    }
+    
+    public interface IImprovement {
+        [DataMember]
+        int Id{get;set;}
+        [DataMember]
+        string Description{get;set;}
+    }
+}
